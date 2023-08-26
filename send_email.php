75f5a342aaeac31a6df04b88e4172df48e99789b
Lucky Dubey <116135685+luckydubey18@users.noreply.github.com>
2023-08-26 18:07:11 +0530

    Add files via upload

---
+++ send_email.php
@@ -0,0 +1,31 @@
+// <?php
+// // Set the recipient email address
+// $to = 'apurv3666@gmail.com';
+//
+// // Get the form data
+// $name = $_POST['name'];
+// $subject = $_POST['subject'];
+// $email = $_POST['email'];
+// $message = $_POST['message'];
+//
+// // Set the email subject and body
+// $subject = 'New message from ' . $name;
+// $body = "Name: $name\n\nSubject: $subject\n\nEmail: $email\n\nMessage: $message";
+//
+// // Set the headers
+// $headers = "From: $name <$email>" . "\r\n";
+// $headers .= "Reply-To: $email" . "\r\n";
+// $headers .= "MIME-Version: 1.0" . "\r\n"; 
+// $headers .= "Content-type: text/plain; charset=utf-8" . "\r\n";
+//
+// // Send the email
+// $mail_sent = mail($to, $subject, $body, $headers);
+//
+// // Check if the email was sent successfully
+// if ($mail_sent) {
+//     echo 'Thank you for your message. We will get in touch with you soon.';
+// } else {
+//     echo 'Sorry, there was a problem sending your message. Please try again later.';
+// }
+// ?>
+    
